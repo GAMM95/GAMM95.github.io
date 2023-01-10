406ffb7d3af616b664209ec4e3da3cdbbf336280
GAMM95 <jhonatanmm.1995@gmail.com>
2023-01-10 12:29:11 -0500

    update

--- js/script.js
+++ js/script.js
@@ -41,3 +41,14 @@ const nav = document.querySelector(".nav"),
         const target = element.getAttribute("href").split("#")[1];
         document.querySelector("#" + target).classList.add("active")
     }
+    const navTogglerBtn = document.querySelector(".nav-toggler"),
+    aside = document.querySelector(".aside");
+    navTogglerBtn.addEventListener("click", () =>
+    {
+        asideSectionTogglerBtn();
+    })
+    function asideSectionTogglerBtn()
+    {
+        aside.classList.toggle("open");
+        navTogglerBtn.classList.toggle("open");
+    }

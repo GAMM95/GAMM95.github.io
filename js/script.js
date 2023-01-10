8afc3f7c43363da320d2db11086183e8851630b6
GAMM95 <jhonatanmm.1995@gmail.com>
2023-01-09 21:58:49 -0500

    update

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========= typing animation =========== */
+var typed = new typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","Youtuber"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
